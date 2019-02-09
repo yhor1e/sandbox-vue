11435429e880258c1e825780ef5dc623596a8ed5
yhorie <yuk.horie@gmail.com>
2019-02-10 01:16:00 +0900

    app5

--- src/main.js
+++ src/main.js
@@ -19,4 +19,27 @@ var app3 = new Vue({
   data: {
     seen: true
   }
-})
+});
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      {text: 'Learn JavaScript'},
+      {text: 'Learn Vue'},
+      {text: 'Build somethind awesome'}
+    ]
+  }
+});
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('');
+    }
+  }
+});
