aeef7cccb90997ad47e7031c6b5e21d365ae2c40
yhorie <yuk.horie@gmail.com>
2019-02-10 00:55:20 +0900

    app2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
+/*global app, app2*/
+
 var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!'
+  }
+});
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
   }
 });
